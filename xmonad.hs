f8551601f911ce6c4a31f0c9bf3591e015b31c53
Adirelle <adirelle@gmail.com>
2014-05-20 13:07:58 +0200

    Move Firefox windows to the Web workspace.

--- xmonad.hs
+++ xmonad.hs
@@ -265,6 +265,7 @@ myManagementHooks = [
   , (className =? "Komodo IDE" <&&> resource =? "Komodo_find2") --> doFloat
   , (className =? "Komodo IDE" <&&> resource =? "Komodo_gotofile") --> doFloat
   , (className =? "Komodo IDE" <&&> resource =? "Toplevel") --> doFloat
+  , (className =? "Firefox") --> doF (W.shift "6:Web")
   , (className =? "Empathy") --> doF (W.shift "7:Chat")
   , (className =? "Pidgin") --> doF (W.shift "7:Chat")
   , (className =? "Gimp-2.8") --> doF (W.shift "9:Pix")
